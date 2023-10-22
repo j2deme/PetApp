13d03dd50ae3b3eebb5ed2341ff85cf208244ab3
j2deme <j2deme@gmail.com>
2023-10-22 16:20:46 -0600

    🗃️ Updates migrations

--- database/migrations/2023_10_22_221636_create_mascotas_table.php
+++ database/migrations/2023_10_22_221636_create_mascotas_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,12 @@ return new class extends Migration
     {
         Schema::create('mascotas', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('especie');
+            $table->date('fecha_nacimiento');
             $table->timestamps();
+            # Cada mascota pertenece a un usuario
+            $table->foreignId('user_id')->constrained();
         });
     }
 
--- database/migrations/2023_10_22_221653_create_mascota_vacunas_table.php
+++ database/migrations/2023_10_22_221653_create_mascota_vacunas_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('mascota_vacunas', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('mascota_id')->constrained();
+            $table->foreignId('vacuna_id')->constrained();
+            $table->date('fecha_aplicacion');
             $table->timestamps();
         });
     }
