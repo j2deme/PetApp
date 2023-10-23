edac8a3d6755d33bf67a9d8afde9443c128d88bd
j2deme <j2deme@gmail.com>
2023-10-22 21:00:12 -0600

    🏗️ Adds routes for resource controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MascotaController;
+use App\Http\Controllers\MascotaVacunaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +22,15 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('mascotas', MascotaController::class);
+    Route::resource('mascotas.vacunas', MascotaVacunaController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
