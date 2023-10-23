b45ca874575df0778fc5b4680235d94e10a622e3
j2deme <j2deme@gmail.com>
2023-10-22 20:55:10 -0600

    ✨ Relations & Fillables on models

--- app/Models/Mascota.php
+++ app/Models/Mascota.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Vacuna;
 
 class Mascota extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nombre', 'especie', 'fecha_nacimiento'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function vacunas()
+    {
+        return $this->belongsToMany(Vacuna::class, 'mascota_vacunas')
+            ->withPivot('fecha_aplicacion')
+            ->withTimestamps();
+    }
 }
--- app/Models/Vacuna.php
+++ app/Models/Vacuna.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mascota;
 
 class Vacuna extends Model
 {
     use HasFactory;
+    protected $fillable = ['nombre', 'fabricante', 'tipo'];
+
+    public function mascotas()
+    {
+        return $this->belongsToMany(Mascota::class, 'mascota_vacunas')
+            ->withPivot('fecha_aplicacion')
+            ->withTimestamps();
+    }
 }
